2775cd2995eb46c882b0fb20efa504d1eca5d09b
CoyBunny <97859310+CoyBunny@users.noreply.github.com>
2023-10-10 08:09:57 +0800

    Update main.go

--- main.go
+++ main.go
@@ -258,8 +258,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	sus = append(sus, 486035121)
-	// sus = append(sus, 87654321)
+
+	 sus = append(sus, 486035121)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
